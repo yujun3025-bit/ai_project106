8da30df340257bea345058ecef4e53d243ed9475
yujun3025-bit <yujun3025@gmail.com>
2026-05-13 13:01:49 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import streamlit as st
 st.title('나의 첫 서비스 만들기!')
 st.text_input('이름을 입력하세요')
-st.selectbox('좋아하는 사람을 선택하세요',['떡볶이','치킨','마라탕'])
+st.selectbox('좋아하는 음식을 선택하세요',['떡볶이','치킨','마라탕'])
 st.button('인사말 생성')
