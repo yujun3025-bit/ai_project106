8aaef987b2e150c48b347634ff261094a274856f
yujun3025-bit <yujun3025@gmail.com>
2026-05-19 13:55:50 +0900

    Update 02_서울관광지.py

--- pages/02_서울관광지.py
+++ pages/02_서울관광지.py
@@ -1,11 +1,1 @@
-import streamlit as st
-import folium
-from streamlit_folium import st_folium
 
-st.set_page_config(
-    page_title="서울 인기 관광지 TOP10",
-    layout="wide"
-)
-
-st.title("📍 외국인들이 좋아하는 서울 주요 관광지 TOP10")
-st.markdown("Folium 지도로 서울의 대표 관광지를 확인하세요.")
